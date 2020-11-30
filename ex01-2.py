d2e2a56e454e39a1dbeda7b2d2666fab06af82cb
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-11-30 20:26:00 -0300

    Create ex01-2.py

---
+++ ex01-2.py
@@ -0,0 +1,16 @@
+# EXERCÍCIO 1.2
+# 1. Quantos segundos há em 42 minutos e 42 segundos?
+
+print(f'Em 42min e 42s tem {42 * 60 + 42} segundos')
+
+# 2. Quantas milhas há em 10 quilometros?
+
+print(f'Em 10Km há {10 / 1.61:.2f}Milhas')
+
+# 3. Se você correr 10 quilometros em 42 minutos e 42 segundo, qual é o seu passo médio?
+
+print(f'Tempo por milha em minutos e segundos: {(42 * 60 + 42) / (10 / 1.61) / 60:.2f}')
+
+# 3.1. Qual é a sua velocidade em milhas por hora?
+
+print(f'Sua velocidades em milhas por hora é {(42 * 60 + 42) / (60*60):.2f}')
