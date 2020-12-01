fd3c970a3f2f0b6c14079420fc11c8f4b372a117
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-11-30 21:06:33 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -14,3 +14,17 @@ p = 24.95 * 40/100
 t = 0.75
 pf = (p * 60) + 3 + (t * 59)
 print(f'O preço final é {pf}')
+
+''' 3. Se eu sair da minha casa às 6:52 e correr 1 quilômetro a um certo passo
+(8min15s porquilômetro), então 3 quilômetros a um passo mais rápido
+(7min12s por quilômetro) e 1 quilômetro no mesmo passo usado em primeiro lugar,
+que horas chego em casa para o café da manhã? '''
+
+import datetime
+
+ti = 60 * 60 * 6 + 52 * 60
+t1 = ti + 8 * 60 + 15
+t2 = t1 + 3 * (7 * 60 + 12)
+t3 = t2 + (8 * 60 + 15)
+
+print(f'Hora: {str(datetime.timedelta(seconds=t3))}')
